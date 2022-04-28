154bb37a1fdb3c54aa83aa36c285e58ddcc700c4
Adharsh <81523460+adharshrj@users.noreply.github.com>
2022-04-28 21:48:10 +0530

    Vision implementation with unit test cases

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var dotenv = require('dotenv')
+var AWS = require('aws-sdk');
 
-router.post('/classify', function(req, res, next) {
+dotenv.config();
+const labellingService = (image) => {
+
+  /*Setting up aws configuration*/
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
+
+  const client = new AWS.Rekognition();
+  const params = {
+      Image: {
+        Bytes: image,
+      },
+      MaxLabels: 10 
+    };
+  
+  return client.detectLabels(params).promise();
+}
+
+router.post('/classify', async function(req, res) {
   // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+  let response = [];
 
   // Your code starts here //
 
+  /* Fetching and reading the image files*/
+  const file = req.files.file;
+  const data = file.data;
+  const image = Buffer.from(data, 'base64');
+
+  try{
+    response = await labellingService(image) 
+    const result = response.Labels.map((label) => label.Name);
+    console.log('Result', result)
+    return res.json({
+      labels: result,
+    });
+
+  } catch (err) {
+    console.error('Error Log', err)
+    return res.status(500).json({ "error": "Unable to process the request" });  
+  }
+ 
   // Your code ends here //
 
-  res.json({
-    "labels": response
-  });
+ 
 });
 
 module.exports = router;
